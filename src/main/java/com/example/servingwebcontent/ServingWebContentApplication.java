51aced64c2580085b8256dff8bc540f83192a325
sunghun <special9486@naver.com>
2020-02-18 13:00:20 +0900

    first commit

---
+++ src/main/java/com/example/servingwebcontent/ServingWebContentApplication.java
@@ -0,0 +1,14 @@
+package com.example.servingwebcontent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.regex.Pattern;
+
+@SpringBootApplication
+public class ServingWebContentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServingWebContentApplication.class, args);
+	}
+}
